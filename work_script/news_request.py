9daccc130e9fe6b728719cfbae1688f2d3b6b13e
Amos.Li <2188356190@qq.com>
2019-04-16 10:07:13 +0800

    test_conn

--- work_script/news_request.py
+++
@@ -1,9 +0,0 @@
-from openpyxl import load_workbook
-
-def open_yu():
-                wb = load_workbook(filename='舆情通数据.xlsx')
-            ws = wb.active
-            news_dict = {}
-            for row in range(2, ws.max_row + 1):
-                area_name = ws[f'i{row}'].value
-                if not area_name:
